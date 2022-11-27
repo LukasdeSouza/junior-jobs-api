5a558d052b06019502bcdfd7cba6b149f367536f
LukasdeSouza <lucasdesouzasilva112@gmail.com>
2022-11-27 16:39:54 -0300

    API funcionando com os principais verbos HTTP

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express')
+require('dotenv').config()
+const mongoose = require('mongoose')
+const { off } = require('./models/Person')
+const app = express()
+
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
+app.use(express.json())
+
+const personRoutes = require('./routes/personRoutes')
+
+app.use('/person', personRoutes)
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Lucas Node API Example' })
+})
+app.get('/customer', (req, res) => {
+  res.json(
+    {
+      customer: {
+        personalInfo: {
+          name: "Lucas",
+          age: 25,
+          country: 'BR',
+          gender: 'Male'
+        },
+        phoneInfo: {
+          DDD: 34,
+          phoneNumber: '99246-1158',
+          operator: 'TIM'
+        },
+        adress: {
+          city: 'Uberlândia',
+          zipCode: '38410-727',
+          neighbourhood: 'Laranjeiras',
+        }
+      },
+      enterprise: {
+        name: 'BestDeal',
+        position: 'Frontend Developer',
+        salary: 'R$4.000',
+        timeInThePosition: '3 Months'
+      }
+    })
+})
+
+const PASSWORD = encodeURIComponent(process.env.DB_PASSWORD)
+mongoose.connect(`mongodb+srv://LukasdeSouza:${PASSWORD}@cluster0.ovsw5ph.mongodb.net/?retryWrites=true&w=majority`)
+  .then(() => {
+    console.log('Conectado ao MongoDB')
+    app.listen(3000)
+  })
+  .catch((err) => console.log(err))
