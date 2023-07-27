2ec16cb6ee19c20f485309216ae4f29cd4431547
LukasdeSouza <lucas@bestdeal.com.br>
2023-07-26 23:19:39 -0300

    fix: correção no erro de cors

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ app.use(express.json())
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT, PATCH, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Referer, Sec-Ch-Ua, Sec-Ch-Ua-Mobile, Sec-Ch-Ua-Platform, User-Agent, Authorization");
   next();
 });
 
