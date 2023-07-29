73de663f5a962407fbfd971543ed2c2e557dd61d
LukasdeSouza <lucas@bestdeal.com.br>
2023-07-29 09:27:39 -0300

    fix: agora funciona hein

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
-const cors = require('cors')
+// const cors = require('cors')
 
 const { off } = require('./models/Person')
 const personRoutes = require('./routes/personRoutes')
