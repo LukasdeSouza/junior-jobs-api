57ccbce16ba645f67fd6290da399931e9c051d22
LukasdeSouza <lucas@bestdeal.com.br>
2023-07-29 09:32:59 -0300

    fix: tentativa de correão de cors dnv

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.json())
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT, PATCH, DELETE");
-  res.header("Access-Control-Allow-Headers", "X-Api-Key", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Headers", "X-Api-Key, Origin, X-Requested-With, Sec-Ch-Ua, Sec-Ch-Ua-Mobile, Sec-Ch-Ua-Platform, Content-Type, Accept, Referer, User-Agent, Authorization");
   next();
 });
 
